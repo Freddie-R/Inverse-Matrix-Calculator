ab0c7a4df38127c66ac727b0b609596cc61c25d1
Freddie R <freddiehread@icloud.com>
2024-05-18 13:52:15 +0100

    Coding #1
    Update 1

---
+++ Initial_Inputs.py
@@ -0,0 +1,53 @@
+def check_val(integer,is_float):
+    if is_float:
+        #check if the interger is a float
+        try:
+            value = float(integer)
+            #if we have an float we return the value
+            return value
+        except ValueError:
+            #if the value can't be cast to an integer we ask for another integer
+            print("Please enter a valid number")
+            return "null"
+    else:
+        #check if the input is an integer 
+        try:
+            value = int(integer)
+            if value < 1:
+                #if needed we check if the integer is less than 1 and if it is we ask for another input
+                print("Please enter a value greater than 0")
+                return "null"
+            #if we have an integer we return the value
+            return value
+        except ValueError:
+            #if the value can't be cast to an integer we ask for another integer
+            print("Please enter a valid integer")
+            return "null"
+
+
+def create_matrix():
+    size = "null"
+    while size == "null":
+        #ask for the size of the matrix until you have a postive interger
+        size = check_val(input("Enter the size of your square matrix: "),False)
+    
+    #define the original matrix
+    matrix = []
+
+    for i in range(size):
+        #create a new row
+        row =[]
+
+        for j in range(size):
+            #for each valye in each row assign a value
+            value = check_val(input(f"Enter the value for cell ({i},{j}): "),True)
+            #append that value to the row
+            row.append(value)
+
+        #append the row to the matrix
+        matrix.append(row)
+
+    #return the matrix
+    return matrix
+
+print(create_matrix())
